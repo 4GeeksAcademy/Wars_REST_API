61826ca666b9ebc377c77f9923cc205f41cfccfe
Adrian Torres <adriantorres.gc@gmail.com>
2024-04-19 10:04:59 +0000

    modified:   src/admin.py 	modified:   src/models.py

--- src/models.py
+++ src/models.py
@@ -17,3 +17,32 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+
+class Planeta(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(120), unique=True, nullable=False)
+    gravedad = db.Column(db.String(80), unique=False, nullable=False)
+    poblacion = db.Column(db.String(80), unique=False, nullable=False)
+    habitable = db.Column(db.String(80), unique=False, nullable=False)
+
+   
+    tipo = db.Column(db.String(250))
+    clima = db.Column(db.String(250))
+    
+    
+
+    def __repr__(self):
+        return '<Planeta %r>' % self.planeta
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "nombre": self.nombre,
+            "gravedad": self.gravedad,
+            "poblacion": self.poblacion,
+            "habitable": self.habitable,
+            "tipo": self.tipo,
+            "clima": self.clima,
+            # do not serialize the password, its a security breach
+        }
